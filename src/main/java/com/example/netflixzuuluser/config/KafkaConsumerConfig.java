26f1f7a650fa4accd50dc9c5bfa40bb5ec01ff4e
김태형 <ktest92@gmail.com>
2023-06-29 08:23:43 +0900

    consumer config 추가

---
+++ src/main/java/com/example/netflixzuuluser/config/KafkaConsumerConfig.java
@@ -0,0 +1,38 @@
+package com.example.netflixzuuluser.config;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.codehaus.jackson.map.deser.std.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        LinkedHashMap<String, Object> properties2 = new LinkedHashMap<>();
+        properties2.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties2.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
+        properties2.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties2.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        Stack<Integer> integerStack = new Stack<>();
+        integerStack.add(1);
+        integerStack.add(2);
+
+        System.out.println("test :"+integerStack.pop());
+        System.out.println("test :"+integerStack.pop());
+
+        return new DefaultKafkaConsumerFactory<>(properties2);
+    }
+}
